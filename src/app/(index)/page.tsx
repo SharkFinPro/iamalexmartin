3860ca85f22e51d8a0eb9c4fbec38acb76d6afe9
Alex Martin <sharkfinpro@hotmail.com>
2024-08-21 17:52:48 -0700

    style setup

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,7 +1,16 @@
+import styles from "./index.module.css";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alex Martin</h1>
+    <div className={styles.wrapper}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.headerContainer}>
+          <h1>Alex Martin</h1>
+        </div>
+      </div>
+      <div>
+        <h2>Hello, World!</h2>
+      </div>
     </div>
   );
 }
