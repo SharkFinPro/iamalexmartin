7735175989f1ad88722ca2475829990d45191681
Alex Martin <sharkfinpro@hotmail.com>
2024-08-21 19:29:53 -0600

    Content styling (#2)
    * Create global.css
    
    * add global styling
    
    * update nextjs
    
    * Update package.json
    
    * style setup
    
    * silly
    
    * Update index.module.css
    
    * beginning of nav overlay
    
    * Update page.tsx
    
    * Update page.tsx
    
    * sample segments

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,7 +1,35 @@
+import styles from "./index.module.css";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Alex Martin</h1>
+    <div className={styles.wrapper}>
+      {/* Header */}
+      <header className={styles.headerWrapper}>
+        <div className={styles.headerContainer}>
+          <h1>Alex Martin</h1>
+        </div>
+      </header>
+
+      {/* About */}
+      <div className={styles.aboutWrapper}>
+        <div className={styles.aboutContainer}>
+          <h2>About</h2>
+        </div>
+      </div>
+
+      {/* Projects */}
+      <div className={styles.projectsWrapper}>
+        <div className={styles.projectsContainer}>
+          <h2>Projects</h2>
+        </div>
+      </div>
+
+      {/* Navigation Overlay */}
+      <nav className={styles.navOverlayWrapper}>
+        <div className={styles.navOverlayContainer}>
+          <a href="">navigation</a>
+        </div>
+      </nav>
     </div>
   );
 }
