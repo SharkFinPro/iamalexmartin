1271db276b0fb1c7698b4a90cfbeefdc692d79ad
Alex Martin <sharkfinpro@hotmail.com>
2024-08-21 18:21:38 -0700

    Update page.tsx

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -3,15 +3,19 @@ import styles from "./index.module.css";
 export default function Page() {
   return (
     <div className={styles.wrapper}>
+      {/* Header */}
       <div className={styles.headerWrapper}>
         <div className={styles.headerContainer}>
           <h1>Alex Martin</h1>
         </div>
       </div>
+
+      {/**/}
       <div>
         <h2>Hello, World!</h2>
       </div>
 
+      {/* Navigation Overlay */}
       <div className={styles.navOverlayWrapper}>
         <div className={styles.navOverlayContainer}>
           <a href="">navigation</a>
