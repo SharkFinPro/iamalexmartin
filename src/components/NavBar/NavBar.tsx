e26651501450b9124abd91de5f186dd0b9e7c3f2
Alex Martin <sharkfinpro@hotmail.com>
2024-08-24 01:33:57 -0700

    fix layering

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <nav className={navBarStyles.wrapper}>
-      <div className={navBarStyles.nameContainer}>
-        <p>Alex Martin</p>
-      </div>
-      <div className={navBarStyles.pagesContainer}>
-        <Link href={""}><p>Test</p></Link>
-        <Link href={""}><p>Test</p></Link>
-        <Link href={""}><p>Test</p></Link>
+      <div className={navBarStyles.container}>
+        <div className={navBarStyles.nameContainer}>
+          <p>Alex Martin</p>
+        </div>
+        <div className={navBarStyles.pagesContainer}>
+          <Link href={""}><p>Test</p></Link>
+          <Link href={""}><p>Test</p></Link>
+          <Link href={""}><p>Test</p></Link>
+        </div>
       </div>
     </nav>
   );
