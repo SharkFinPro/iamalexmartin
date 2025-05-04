2e1abb9176b55eb9c57e24fdf1c53850400f42b1
Alex Martin <sharkfinpro@hotmail.com>
2025-05-04 00:11:30 -0700

    split up segments into files

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,51 +1,11 @@
 import styles from "./index.module.scss";
-import Link from "next/link";
+import Landing from "./Landing";
+import Portfolio from "./Portfolio";
 
 export default function Page() {
-  return (
-    <div>
-      <div className={`${styles.wrapper} ${styles.homepage}`}>
-        <div className={styles.container}>
-          <h1 className={styles.title}>Alexander Martin</h1>
-          <p className={styles.description}>Full-stack developer specializing in interactive graphics and modern web systems, focusing on performance, accuracy, and clean, maintainable architecture.</p>
+  return <>
+    <Landing className={`${styles.wrapper} ${styles.homepage}`} />
 
-          <div className={styles.buttons}>
-            <Link href={"/projects"} className={`${styles.homepageButton} ${styles.viewProjects}`}>View My Work</Link>
-            <Link href={"/contact"} className={`${styles.homepageButton} ${styles.contactMe}`}>Contact Me</Link>
-          </div>
-        </div>
-      </div>
-
-      <div className={`${styles.wrapper} ${styles.welcome}`}>
-        <h2>Welcome to My Portfolio</h2>
-        <p>Explore my work and skills</p>
-
-        <div className={styles.quickLinks}>
-          <div className={styles.quickLinkCard}>
-            <div className={styles.quickLinkIcon}>🌌️</div>
-            <h3>Graphics Programming</h3>
-            <p>Check out my work in high-performance graphics using C++, Vulkan, and GLSL to create stunning visual experiences.</p>
-
-            <Link href={"/projects"}>View Projects</Link>
-          </div>
-
-          <div className={styles.quickLinkCard}>
-            <div className={styles.quickLinkIcon}>💻</div>
-            <h3>Web Development</h3>
-            <p>Browse my web development projects built with React, Next.js, and other modern frameworks for dynamic, interactive web apps.</p>
-
-            <Link href={"/projects"}>View Projects</Link>
-          </div>
-
-          <div className={styles.quickLinkCard}>
-            <div className={styles.quickLinkIcon}>📝</div>
-            <h3>Technical Blog</h3>
-            <p>Read articles where I share insights, experiments, and lessons learned across various computer science topics.</p>
-
-            <Link href={"/blog"}>View Projects</Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    <Portfolio className={`${styles.wrapper} ${styles.welcome}`} />
+  </>
 }
