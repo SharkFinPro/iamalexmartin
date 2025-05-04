985afac53a4335c5a7071527272bf67699e77ac2
Alex Martin <sharkfinpro@hotmail.com>
2025-05-04 11:03:35 -0700

    add banner to pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,7 @@
+import Banner from "@/components/Banner";
+
 export default function Page() {
   return <>
-
+    <Banner title={"Blog"} description={"Graphics programming insights, tutorials, and thoughts on technology"} />
   </>
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
+import Banner from "@/components/Banner";
+
 export default function Page() {
   return <>
-
+    <Banner title={"Contact Me"} description={"Let's connect and discuss potential opportunities"} />
   </>
 }
