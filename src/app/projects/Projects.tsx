3cb82c4dc69ac8867479d2f05130944b4b463ad3
Alex Martin <sharkfinpro@hotmail.com>
2025-05-19 21:47:54 -0700

    add project type selector

--- src/app/projects/Projects.tsx
+++ src/app/projects/Projects.tsx
@@ -1,3 +1,4 @@
+"use client";
 import styles from "./projects.module.scss";
 import Link from "next/link";
 import Image from "next/image";
@@ -7,6 +8,7 @@ import vkImg from "../../public/images/vk.png";
 import ecsImg from "../../public/images/ecs.png";
 import platformerImg from "../../public/images/platformer.png";
 import alaskaclaveImg from "../../public/images/alaskaclave.png";
+import { useState } from "react";
 
 function ProjectCard({ project }) {
   return (
@@ -44,43 +46,64 @@ export default function Projects() {
       name: "ECS3D",
       description: "A modular 3D game engine with an Entity Component System architecture, physics simulation, and Vulkan rendering.",
       tags: ["C++", "Vulkan", "ECS", "Physics"],
-      image: ecsImg
+      image: ecsImg,
+      type: "graphics"
     },
     {
       name: "Vulkan Renderer",
       description: "High-performance real-time 3D renderer with compute and graphics pipelines for optimized particle simulation.",
       tags: ["C++", "Vulkan", "GLSL", "Compute Shaders"],
-      image: vkImg
+      image: vkImg,
+      type: "graphics"
     },
     {
       name: "Conclave Website",
       description: "Official event website built with Gatsby, CSS, and Netlify, serving over 100 attendees with dynamic content.",
       tags: ["Gatsby", "CSS", "Contentful", "MongoDB"],
-      image: alaskaclaveImg
+      image: alaskaclaveImg,
+      type: "web"
     },
     {
       name: "Trading Post Web App",
       description: "Full-stack application with Next.js, React, and SQL to streamline order tracking and inventory management.",
       tags: ["Next.js", "React", "SQL", "Admin Dashboard"],
-      image: tpImg
+      image: tpImg,
+      type: "web"
     },
     {
       name: "Platformer",
       description: "A 2D Platformer Built with a Custom C++ Entity Framework and SFML",
       tags: ["C++", "SFML", "Platformer"],
-      image: platformerImg
+      image: platformerImg,
+      type: "graphics"
     }
   ];
 
+  const [projectType, setProjectType] = useState("all");
+
   return (
     <div className={styles.container}>
-      {/*<h3>All Projects</h3>*/}
-      <h3></h3>
+      <div className={styles.projectTypeSelector}>
+        <button className={projectType === "all" ? styles.selectedProjectType : ""}
+                onClick={()=>setProjectType("all")}>
+          All Projects
+        </button>
+        <button className={projectType === "graphics" ? styles.selectedProjectType : ""}
+                onClick={()=>setProjectType("graphics")}>
+          Graphics
+        </button>
+        <button className={projectType === "web" ? styles.selectedProjectType : ""}
+                onClick={()=>setProjectType("web")}>
+          Web Development
+        </button>
+      </div>
 
       <div className={styles.cards}>
-        {projects.map((project) => (
-          <ProjectCard project={project} key={project.name} />
-        ))}
+        {projects
+          .filter(project => projectType === "all" || projectType === project.type)
+          .map((project) => (
+            <ProjectCard project={project} key={project.name}/>
+          ))}
       </div>
     </div>
   );
