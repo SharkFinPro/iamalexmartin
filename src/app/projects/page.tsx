555c6446504db515cf9f1e078ae7075d01f57d0b
Alex Martin <sharkfinpro@hotmail.com>
2025-05-20 10:00:37 -0700

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,13 @@
 import Banner from "@/components/Banner";
 import Projects from "./Projects";
+import { Suspense } from "react";
 
 export default function Page() {
   return <>
     <Banner title={"My Projects"} description={"Explore my technical work and creative solutions"} />
 
-    <Projects />
+    <Suspense>
+      <Projects />
+    </Suspense>
   </>
 }
