e38e2f9d043c9b9225593c5ffad2f07c8382c21b
Alex Martin <sharkfinpro@hotmail.com>
2025-05-27 09:58:47 -0700

    add page titles

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,11 @@
 import Banner from "@/components/Banner";
 import Projects from "./Projects";
 import { Suspense } from "react";
+import type { Metadata } from "next";
+
+export const metadata : Metadata = {
+  title: "Projects"
+};
 
 export const dynamic = "force-dynamic";
 
