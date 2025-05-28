4c0d4899c1152aa572ab19cfe33b794b636bf3be
Alex Martin <sharkfinpro@hotmail.com>
2025-05-28 12:07:56 -0700

    test content

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,13 +1,42 @@
-"use client";
-import { useParams } from "next/navigation";
+import { RichText } from '@graphcms/rich-text-react-renderer';
+import styles from './RichText.module.scss';
 
-export default function Page() {
-  const params = useParams();
+const CONTENT_QUERY = `
+  query Tests {
+    tests {
+      rich {
+        raw
+      }
+    }
+  }
+`;
 
-  const { slug } = params;
+async function getContent() {
+  const response = await fetch(process.env.CMS_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: CONTENT_QUERY
+    })
+  });
+  const json = await response.json();
+
+  return json.data.tests[0].rich.raw;
+}
+
+export default async function Page({ params }) {
+  const { slug } = await params;
+
+  const content = await getContent();
 
   return <>
     <h3>Hello, World!</h3>
     <p>{slug}</p>
+    {/* @ts-ignore */}
+    <div className={styles.container}>
+      <RichText content={content} />
+    </div>
   </>
 }
