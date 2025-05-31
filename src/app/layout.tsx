fd6dc504d0ee8dc04369207ddbc13cede2eab11b
Alex Martin <sharkfinpro@hotmail.com>
2025-05-30 18:38:52 -0700

    Bug Fixes (#9)
    * ensure project cards have correct height
    
    * fix card hover animation
    
    * improve font loading
    
    * update next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,13 +7,20 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
+import { Open_Sans } from "next/font/google";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap"
+})
+
 export default function RootLayout({
   children
 }: {
   children: ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={openSans.className}>
       <body>
         <NavBar />
         {children}
