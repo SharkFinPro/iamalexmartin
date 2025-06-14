b65f7fa234dfc94b30c490b4af06c59c12263ea5
Alex Martin <sharkfinpro@hotmail.com>
2025-06-14 16:54:09 -0700

    use css only

--- src/app/(index)/Portfolio.tsx
+++ src/app/(index)/Portfolio.tsx
@@ -1,33 +1,13 @@
-"use client";
 import styles from "./portfolio.module.scss";
 import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-import { useState, useEffect } from "react";
 
 library.add(fas);
 
 export default function Portfolio({ className, cards, description }) {
-  const [isMobile, setIsMobile] = useState(false);
-
-  // Check if screen width is less than 500px
-  useEffect(() => {
-    const checkScreenSize = () => {
-      setIsMobile(window.innerWidth < 500);
-    };
-
-    // Initial check
-    checkScreenSize();
-
-    // Add event listener for window resize
-    window.addEventListener("resize", checkScreenSize);
-
-    // Clean up event listener
-    return () => window.removeEventListener("resize", checkScreenSize);
-  }, []);
-
   return (
     <div className={className}>
       <h2 className={styles.sectionHeader}>{description.header}</h2>
@@ -40,7 +20,8 @@ export default function Portfolio({ className, cards, description }) {
               <FontAwesomeIcon icon={['fas', fontAwesomeIcon]} />
             </div>
             <h3>{title}</h3>
-            <p>{isMobile ? shortDescription : description}</p>
+            <p className={styles.description}>{description}</p>
+            <p className={styles.shortDescription}>{shortDescription}</p>
 
             <Link href={link}>{linkText}</Link>
           </div>
