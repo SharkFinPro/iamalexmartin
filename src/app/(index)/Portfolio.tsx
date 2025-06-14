3bb6a36cf8de0a393e4d30d1a355756e3b1589eb
Alex Martin <sharkfinpro@hotmail.com>
2025-06-13 21:59:31 -0700

    Dynamically load portfolio cards

--- src/app/(index)/Portfolio.tsx
+++ src/app/(index)/Portfolio.tsx
@@ -3,10 +3,13 @@ import styles from "./portfolio.module.scss";
 import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCube, faDesktop, faFileCode } from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from "react";
 
-export default function Portfolio({ className }) {
+library.add(fas);
+
+export default function Portfolio({ className, cards }) {
   const [isMobile, setIsMobile] = useState(false);
 
   // Check if screen width is less than 500px
@@ -25,43 +28,18 @@ export default function Portfolio({ className }) {
     return () => window.removeEventListener("resize", checkScreenSize);
   }, []);
 
-  const cards = [
-    {
-      icon: <FontAwesomeIcon icon={faCube} />,
-      header: "Graphics Programming",
-      description: "Check out my work in high-performance graphics using C++, Vulkan, and GLSL to create stunning visual experiences.",
-      shortDescription: "Real-time rendering engines and visual simulations",
-      link: "/projects?projectType=graphics",
-      linkText: "View Projects"
-    },
-    {
-      icon: <FontAwesomeIcon icon={faDesktop} />,
-      header: "Web Development",
-      description: "Browse my web development projects built with React, Next.js, and other modern frameworks for dynamic, interactive web apps",
-      shortDescription: "Interactive websites and full-stack web apps",
-      link: "/projects?projectType=web",
-      linkText: "View Projects"
-    },
-    {
-      icon: <FontAwesomeIcon icon={faFileCode} />,
-      header: "Technical Blog",
-      description: "Read articles where I share insights, experiments, and lessons learned across various computer science topics.",
-      shortDescription: "Insights on graphics, systems, and development",
-      link: "/blog",
-      linkText: "Read Articles"
-    }
-  ];
-
   return (
     <div className={className}>
       <h2 className={styles.sectionHeader}>Welcome to My Portfolio</h2>
       <p className={styles.sectionDescription}>Explore my work and skills</p>
 
       <div className={styles.quickLinks}>
-        {cards.map(({ icon, header, description, shortDescription, link, linkText}) => (
-          <div key={header} className={styles.quickLinkCard}>
-            <div className={styles.quickLinkIcon}>{icon}</div>
-            <h3>{header}</h3>
+        {cards.map(({ fontAwesomeIcon, title, description, shortDescription, link, linkText}) => (
+          <div key={title} className={styles.quickLinkCard}>
+            <div className={styles.quickLinkIcon}>
+              <FontAwesomeIcon icon={['fas', fontAwesomeIcon]} />
+            </div>
+            <h3>{title}</h3>
             <p>{isMobile ? shortDescription : description}</p>
 
             <Link href={link}>{linkText}</Link>
--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -7,10 +7,36 @@ export const metadata : Metadata = {
   title: "Portfolio"
 };
 
-export default function Page() {
+const PORTFOLIO_QUERY = `
+  query PortfolioCards {
+    portfolioCards {
+      title
+      fontAwesomeIcon
+      description
+      shortDescription
+      linkText
+      link
+    }
+  }
+`;
+
+export default async function Page() {
+  const portfolioCardsRequest = await fetch(process.env.CMS_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: PORTFOLIO_QUERY
+    })
+  });
+
+  const portfolioCards = await portfolioCardsRequest.json();
+
+
   return <>
     <Landing className={`${styles.wrapper} ${styles.homepage}`} />
 
-    <Portfolio className={`${styles.wrapper} ${styles.welcome}`} />
+    <Portfolio className={`${styles.wrapper} ${styles.welcome}`} cards={portfolioCards.data.portfolioCards} />
   </>
 }
