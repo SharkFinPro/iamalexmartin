fdf0768fd77d92ce8f85b3fcc21d1378459687dd
Alex Martin <sharkfinpro@hotmail.com>
2025-06-16 13:03:40 -0700

    Revert "Better mobile sizing"
    This reverts commit 61fc7f00876933e50684f5b67b4fe1c056150d18.

--- src/app/contact/sendMessage.ts
+++ src/app/contact/sendMessage.ts
@@ -93,25 +93,11 @@ function createEmailData(name : string, email : string, subject : string, messag
                   border-radius: 4px;
                   padding: 20px; 
                   margin: 25px 0;
-                  
-                  & > h3 {
-                    margin-top: 0;
-                    color: #495057;
-                    font-size: 16px;
-                  }
                 }
-                .message-detail {
-                  display: grid;
-                  box-sizing: border-box;
-                  margin-bottom: 0.5rem;
-                  
-                  @media(min-width: 600px) {
-                    grid-template-columns: auto 1fr;
-                  }
-                  
-                  & > * {
-                    padding: 0;
-                  }
+                .message-details h3 {
+                  margin-top: 0;
+                  color: #495057;
+                  font-size: 16px;
                 }
                 .message-text {
                   color: #6c757d;
@@ -175,19 +161,19 @@ function createEmailData(name : string, email : string, subject : string, messag
                   <div class="message-details">
                     <h3>Message Details</h3>
                     <table>
-                      <tr class="message-detail">
+                      <tr>
                         <td class="label">From:</td>
                         <td>${name}</td>
                       </tr>
-                      <tr class="message-detail">
+                      <tr>
                         <td class="label">Email:</td>
                         <td>${email}</td>
                       </tr>
-                      <tr class="message-detail">
+                      <tr>
                         <td class="label">Date:</td>
                         <td>${dateString}</td>
                       </tr>
-                      <tr class="message-detail">
+                      <tr>
                         <td class="label">Subject:</td>
                         <td>${subject}</td>
                       </tr>
